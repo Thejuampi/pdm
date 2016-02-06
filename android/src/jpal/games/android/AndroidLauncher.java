a69857b6bc9e1530c2ace9c372b7d2972950cf57
Juan <juanpablo.lescano@gmail.com>
2016-02-06 18:02:25 -0300

    Primer Commit

---
+++ android/src/jpal/games/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package jpal.games.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import jpal.games.BounceandoGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new BounceandoGame(), config);
+	}
+}
---
+++ desktop/src/jpal/games/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package jpal.games.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import jpal.games.BounceandoGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BounceandoGame(), config);
+	}
+}
